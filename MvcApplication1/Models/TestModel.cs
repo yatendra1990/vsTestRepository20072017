d930b6da0ed4e8d99998c3bb97451f4160ec73f6
yatendra1990 <ysharma@smartapp.com>
2017-07-20 14:26:45 +0530

    New Model and View created

---
+++ MvcApplication1/Models/TestModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class TestModel
+    {
+        private int id;
+        private string name;
+
+        public int ID {
+            get { return id; }
+
+            set { id = value; }
+        }
+        public string Name {
+
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
